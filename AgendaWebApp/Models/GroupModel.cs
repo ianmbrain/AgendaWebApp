bfe025e327211b3d4eaf0a2e4bc0e557ba54335b
ian brain <ianbrain080@gmail.com>
2023-05-07 10:13:36 -0400

    Docs:  Added comments to Group class
    Added comments to Group class

--- AgendaWebApp/Models/GroupModel.cs
+++ AgendaWebApp/Models/GroupModel.cs
@@ -5,17 +5,37 @@ namespace AgendaWebApp.Models
 {
     public class GroupModel
     {
+        /// <summary>
+        /// Primary key of group
+        /// </summary>
         [Key]
         public int Id { get; set; }
 
+        /// <summary>
+        /// Group Id
+        /// </summary>
+        public int GroupId { get; set; }
+
+        /// <summary>
+        /// Name of group
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Description of group
+        /// </summary>
         public string Description { get; set; }
 
+        /// <summary>
+        /// Todo items associated with the group
+        /// </summary>
         [ForeignKey("TodoItemModel")]
         public TodoItemModel TodoItem { get; set; }
 
+        /// <summary>
+        /// App users associated with the group
+        /// </summary>
         [ForeignKey("AppUser")]
-        public AppUser AppUser { get; set; }
+        public AppUser? AppUser { get; set; }
     }
 }
