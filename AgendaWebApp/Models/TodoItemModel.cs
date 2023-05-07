027a3d45db821ccd96dcb05ef75f95c97bdc3e16
ian brain <ianbrain080@gmail.com>
2023-05-07 09:53:21 -0400

    Created TodoItem model representing a to do item in the application.

---
+++ AgendaWebApp/Models/TodoItemModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendaWebApp.Models
+{
+    public class TodoItemModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Importance { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime FinishedDate { get; set; }
+
+        public bool Finished { get; set; }
+    }
+}
