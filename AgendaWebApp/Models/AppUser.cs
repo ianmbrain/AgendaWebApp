5ae22eb8eec7e3072c375de41a1572d3620242b6
ian brain <ianbrain080@gmail.com>
2023-05-07 09:56:38 -0400

    Created AppUser model representing users of the application.

---
+++ AgendaWebApp/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AgendaWebApp.Models
+{
+    public class AppUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
