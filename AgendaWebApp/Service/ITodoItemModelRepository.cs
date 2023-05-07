e3fc793dbe94c03b869495162ded7ef9d9233ae6
ian brain <ianbrain080@gmail.com>
2023-05-07 15:49:57 -0400

    Feat: Added interface for database dependency injection
    Added interface for database dependency injection

---
+++ AgendaWebApp/Service/ITodoItemModelRepository.cs
@@ -0,0 +1,16 @@
+﻿using AgendaWebApp.Models;
+
+namespace AgendaWebApp.Service
+{
+    public interface ITodoItemModelRepository
+    {
+        Task<IEnumerable<TodoItemModel>> GetAll();
+        Task<TodoItemModel> GetByIdAsync(int id);
+        Task<IEnumerable<TodoItemModel>> GetItemByImportance(String importance);
+        bool Add(TodoItemModel item);
+        bool Delete(TodoItemModel item);
+        bool Save();
+
+
+    }
+}
