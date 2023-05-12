c4326f32bc8b9f16b6cb7fd2ee5b42ac8369cd4d
ian brain <ianbrain080@gmail.com>
2023-05-12 06:57:12 -0400

    Test: Created test class for the to do item controller

---
+++ AgendaWebApp.Tests/Controller.Test/TodoItemControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using AgendaWebApp.Service;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaWebApp.Tests.Controller.Test
+{
+    public class TodoItemControllerTests
+    {
+        private ITodoItemModelRepository _context;
+
+        public TodoItemControllerTests() {
+            // Dependency so that the tests are run on mocked data rather than the production database
+            _context = A.Fake<ITodoItemModelRepository>();
+        }
+    }
+}
