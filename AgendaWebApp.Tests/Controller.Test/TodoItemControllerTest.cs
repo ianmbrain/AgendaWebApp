c361b58ab597428cb79d6b1159283c110600e7eb
ian brain <ianbrain080@gmail.com>
2023-06-04 11:05:20 -0400

    Test: Added and updated tests for todoItem controller.

--- AgendaWebApp.Tests/Controller.Test/TodoItemControllerTest.cs
+++ AgendaWebApp.Tests/Controller.Test/TodoItemControllerTest.cs
@@ -1,13 +1,17 @@
 ﻿using AgendaWebApp.Controllers;
+using AgendaWebApp.Data;
 using AgendaWebApp.Models;
 using AgendaWebApp.Service;
 using AgendaWebApp.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,14 +19,53 @@ namespace AgendaWebApp.Tests.Controller.Test
 {
     public class TodoItemControllerTest
     {
+
         [Fact]
-        public void GetAll_ReturnsAViewResult_ListOfTasks()
+        public void GetAll_ReturnsAViewResult_ListOfTasksNoUserLoggedIn()
         {
-            // Arrange
+            //Arrange
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            httpContextAccessorMock.Setup(x => x.HttpContext.User.Identity.IsAuthenticated).Returns(false);
+
             var mockRepo = new Mock<ITodoItemModelRepository>();
             mockRepo.Setup(repo => repo.GetAll())
                 .Returns(GetTodoItems());
+
             var controller = new TodoItemModelController(mockRepo.Object);
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = httpContextAccessorMock.Object.HttpContext;
+
+            // Act
+            var result = controller.AllTasks();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal("Views/GroupModel/NoLoginView.cshtml", viewResult.ViewName);
+        }
+
+            [Fact]
+        public void GetAll_ReturnsAViewResult_ListOfTasks()
+        {
+            // Arrange
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "test@test.com"),
+            }, "mock"));
+
+            var httpContext = new DefaultHttpContext();
+            httpContext.User = user;
+
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            httpContextAccessorMock.Setup(x => x.HttpContext).Returns(httpContext);
+
+            var mockRepo = new Mock<ITodoItemModelRepository>();
+            mockRepo.Setup(repo => repo.GetAll())
+                // Tasks are ordered by importance as that is how the repository orders them.
+                .Returns(GetTodoItems().OrderByDescending(model => model.Importance).ToList());
+
+            var controller = new TodoItemModelController(mockRepo.Object);
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = httpContextAccessorMock.Object.HttpContext;
 
             // Act
             var result = controller.AllTasks();
@@ -55,7 +98,7 @@ namespace AgendaWebApp.Tests.Controller.Test
         }
 
         [Fact]
-        public void CreatePost_ReturnsAViewResult_CreateOneTask()
+        public void CreatePost_ReturnsAViewResult_CreateTaskNoGroup()
         {
             // Arrange
             var mockRepo = new Mock<ITodoItemModelRepository>();
@@ -79,7 +122,7 @@ namespace AgendaWebApp.Tests.Controller.Test
                 Id = 1,
                 Name = "Name1",
                 Description = "Description1",
-                Importance = 0,
+                Importance = (Data.Enum.ImportanceEnum)0,
                 CreationDate = new DateTime(2023, 5, 1),
                 GroupModelId = 1
             });
@@ -88,7 +131,7 @@ namespace AgendaWebApp.Tests.Controller.Test
                 Id = 2,
                 Name = "Name2",
                 Description = "Description2",
-                Importance = 0,
+                Importance = (Data.Enum.ImportanceEnum)1,
                 CreationDate = new DateTime(2023, 5, 1),
                 GroupModelId = 1
             });
@@ -99,7 +142,6 @@ namespace AgendaWebApp.Tests.Controller.Test
         {
             return new CreateTodoItemViewModel
             {
-                Id = 1,
                 Name = "Name1",
                 Description = "Description1",
                 Importance = 0,
