6f2f10ed9d3d0a2baaffb21a7f9f0fb65e2a6d49
ian brain <ianbrain080@gmail.com>
2023-06-04 14:07:23 -0400

    Test: Completed tests for all of the correct paths of the todo model controller actions.

--- AgendaWebApp.Tests/Controller.Test/TodoItemControllerTest.cs
+++ AgendaWebApp.Tests/Controller.Test/TodoItemControllerTest.cs
@@ -1,10 +1,13 @@
 ﻿using AgendaWebApp.Controllers;
 using AgendaWebApp.Data;
+using AgendaWebApp.Data.Enum;
 using AgendaWebApp.Models;
 using AgendaWebApp.Service;
 using AgendaWebApp.ViewModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using Microsoft.CodeAnalysis.Elfie.Model.Structures;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -98,11 +101,21 @@ namespace AgendaWebApp.Tests.Controller.Test
         }
 
         [Fact]
-        public void CreatePost_ReturnsAViewResult_CreateTaskNoGroup()
+        public void CreatePost_RedirectToAction_CreateTask()
         {
             // Arrange
+            var todoItem = new TodoItemModel
+            {
+                Id = 1,
+                Name = "Name1",
+                Description = "Description1",
+                Importance = 0,
+                GroupModelId = 1
+            };
+
             var mockRepo = new Mock<ITodoItemModelRepository>();
-            mockRepo.Setup(repo => repo.Add(GetTodoItems().ElementAt(0)));
+            mockRepo.Setup(repo => repo.Add(todoItem))
+                .Returns(true);
             var controller = new TodoItemModelController(mockRepo.Object);
 
             // Act
@@ -110,8 +123,120 @@ namespace AgendaWebApp.Tests.Controller.Test
 
             // Assert
             var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
-            Assert.Null(redirectToActionResult.ControllerName);
             Assert.Equal("GetTasksByGroupId", redirectToActionResult.ActionName);
+        }
+
+        [Fact]
+        public void CreateNoGroupPost_RedirectToAction_CreateTask()
+        {
+            // Arrange
+            var todoItem = new TodoItemModel
+            {
+                Id = 1,
+                Name = "Name1",
+                Description = "Description1",
+                Importance = 0,
+                GroupModelId = 1
+            };
+
+            var mockRepo = new Mock<ITodoItemModelRepository>();
+            mockRepo.Setup(repo => repo.Add(todoItem))
+                .Returns(true);
+            var controller = new TodoItemModelController(mockRepo.Object);
+
+            // Act
+            var result = controller.Create(CreateViewModel());
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("GetTasksByGroupId", redirectToActionResult.ActionName);
+        }
+
+        [Fact]
+        public void EditGet_ReturnsAEditViewModel()
+        {
+            // Arrange
+            var mockRepo = new Mock<ITodoItemModelRepository>();
+            mockRepo.Setup(repo => repo.GetById(1))
+                .Returns(GetTodoItems().ElementAt(0));
+            var controller = new TodoItemModelController(mockRepo.Object);
+
+            // Act
+            var result = controller.Edit(1);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<EditItemViewModel>(
+                viewResult.ViewData.Model);
+            Assert.Equal(1, model.Id);
+            Assert.Equal("Name1", model.Name);
+            Assert.Equal("Description1", model.Description);
+        }
+
+        [Fact]
+        public void EditPost_RedirectToAction_UpdatedTask()
+        {
+            // Arrange
+            var mockRepo = new Mock<ITodoItemModelRepository>();
+            mockRepo.Setup(repo => repo.GetByIdNoTracking(1))
+                .Returns(GetTodoItems().ElementAt(0));
+            mockRepo.Setup(repo => repo.Update(GetTodoItems().ElementAt(0)))
+                .Returns(true);
+            var controller = new TodoItemModelController(mockRepo.Object);
+
+            var editViewModel = new EditItemViewModel
+            {
+                Id = 1,
+                Name = "Name1",
+                Description = "Description1",
+                Importance = 0,
+                GroupModelId = 1
+            };
+
+            // Act
+            var result = controller.Edit(1, editViewModel);
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("GetTasksByGroupId", redirectToActionResult.ActionName);
+        }
+
+        [Fact]
+        public void Delete_RedirectToAction_DeleteTask()
+        {
+            // Arrange
+            var mockRepo = new Mock<ITodoItemModelRepository>();
+            mockRepo.Setup(repo => repo.GetById(1))
+                .Returns(GetTodoItems().ElementAt(0));
+            mockRepo.Setup(repo => repo.Delete(GetTodoItems().ElementAt(0)))
+                .Returns(true);
+            var controller = new TodoItemModelController(mockRepo.Object);
+
+            // Act
+            var result = controller.Delete(1);
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectToActionResult.ActionName);
+        }
+
+        [Fact]
+        public void Finish_RedirectToAction_FinishTask()
+        {
+            // Arrange
+            var mockRepo = new Mock<ITodoItemModelRepository>();
+            mockRepo.Setup(repo => repo.GetById(1))
+                .Returns(GetTodoItems().ElementAt(0));
+            mockRepo.Setup(repo => repo.Update(GetTodoItems().ElementAt(0)))
+                .Returns(true);
+            var controller = new TodoItemModelController(mockRepo.Object);
+
+            // Act
+            var result = controller.Finish(1);
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectToActionResult.ActionName);
         }
 
         private List<TodoItemModel> GetTodoItems()
@@ -145,7 +270,6 @@ namespace AgendaWebApp.Tests.Controller.Test
                 Name = "Name1",
                 Description = "Description1",
                 Importance = 0,
-                CreationDate = new DateTime(2023, 5, 1),
                 GroupModelId = 1
             };
         }
